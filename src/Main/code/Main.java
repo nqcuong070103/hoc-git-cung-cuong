9f2b2f92d16e92826e1f33589c9f40778edb3e2a
Phong Phan <phongig007@gmail.com>
2026-03-24 00:12:49 +0700

    cong dong

--- src/Main/code/Main.java
+++ src/Main/code/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] thamso){
         System.out.println("Hello.Phat");
-        System.out.println("Dm Nhan");
+        System.out.println("Nhan gay");
     }
 }
